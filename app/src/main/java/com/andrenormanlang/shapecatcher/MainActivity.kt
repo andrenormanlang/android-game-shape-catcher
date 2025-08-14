6c8844b772ae5b5e2f1c8424d625e13ddd95ef3f
andrenormanlang <andrenormanlang@gmail.com>
2025-08-14 09:59:35 +0200

    Initial commit

---
+++ app/src/main/java/com/andrenormanlang/shapecatcher/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.andrenormanlang.shapecatcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.andrenormanlang.shapecatcher.ui.theme.ShapeCatcherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShapeCatcherTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "mainMenu") {
+                        composable("mainMenu") {
+                            MainMenuScreen(
+                                onStartGame = { navController.navigate("gameScreen") }
+                            )
+                        }
+                        composable("gameScreen") {
+                            // Assuming GameScreen is in the same package or imported correctly
+                            GameScreen() 
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainMenuScreen(modifier: Modifier = Modifier, onStartGame: () -> Unit) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Shape Catcher",
+            fontSize = 32.sp,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        Button(
+            onClick = onStartGame,
+            modifier = Modifier.fillMaxWidth(0.7f)
+        ) {
+            Text("Start Game", fontSize = 20.sp)
+        }
+        // TODO: Add "View Scores" button later
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainMenuScreenPreview() {
+    ShapeCatcherTheme {
+        MainMenuScreen(onStartGame = {})
+    }
+}
